c1562cb5c97b286cdfe701a89e9d7fdd0ff72a9f
James Phillips <jphillips@fnni.com>
2021-10-13 15:18:55 -0700

    updated superfluous variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,3 @@
-variable "admin_sso_role" {
-  type = string
-}
-variable "ci-user" {
-  type = string
-}
-variable "costcenter" {
-  type = string
-}
 variable "organization" {
   type = string
 }
