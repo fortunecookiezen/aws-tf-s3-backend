b6ba2e6dac51d934f272a27fbab934511b90222a
James Phillips <phillips.james@gmail.com>
2022-04-05 18:14:14 -0700

    refactored resources to support v4 aws provider

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "backend" {
-  source       = "git::https://github.com/fortunecookiezen/aws-tf-s3-backend.git"
+  source       = "git::https://github.com/fortunecookiezen/aws-tf-s3-backend.git?ref=update_v4_provider"
   organization = "demo"
   environment  = "test"
 }
