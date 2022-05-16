6edaca89ce5b81b0b896afaedafcb543aacd95e1
James Phillips <jphillips@fnni.com>
2022-05-16 16:19:46 -0500

    added public access block resource to terraform state bucket

--- main.tf
+++ main.tf
@@ -15,6 +15,15 @@ resource "aws_s3_bucket" "terraform-backend" {
     Environment  = var.environment
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.terraform-backend.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.terraform-backend.id
   versioning_configuration {
